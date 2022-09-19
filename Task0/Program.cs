19ce14403e95d7540c47df9ebb90e98229c17969
Dmitrii <razuhindm@gmail.com>
2022-09-19 20:23:32 +0300

    Первая задача

---
+++ Task0/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+Clear();
+//Write("Введите число: ");
+int number = new Random().Next(10,100);
+WriteLine(number);
+
+int a0 = number%10;
+int a1 = number/10;
+
+if (a0>a1)
+{
+    WriteLine($"Наибольшая цифра: {a0}");
+}
+else
+{
+    WriteLine($"Наибольшая цифра: {a1}");
+}
+
