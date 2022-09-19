e0afa56ac820f338df069827d508886173439840
Dmitrii <razuhindm@gmail.com>
2022-09-19 20:37:52 +0300

    Вторая задача

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+Clear();
+int number = new Random().Next(100,1000);
+WriteLine(number);
+
+int a0 = number%10;
+//int a1 = number/10;
+int a2 = number/100;
+
+int result = a2*10+a0;
+
+//WriteLine($"{a2}{a0}");
+WriteLine(result);
+
