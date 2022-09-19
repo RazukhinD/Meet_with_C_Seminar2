5c9d09b795e8fc76966e38f9d9cb2526296aef35
Dmitrii <razuhindm@gmail.com>
2022-09-19 22:17:30 +0300

    Добавил в задачу с переворотом н мерного числа, создание этого числа через Рандом

---
+++ Task7/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+Clear();
+WriteLine("Введи количество разрядов n: ");
+int lenght = int.Parse(ReadLine()!);
+int number = new Random().Next(Convert.ToInt32(Math.Pow(10,lenght-1)),Convert.ToInt32(Math.Pow(10,lenght)));
+WriteLine(number);
+double new_number=0;
+double coef = Math.Pow(10, lenght-1);
+while (number != 0)
+{
+    new_number += (number % 10) * coef;
+    number /= 10;
+    coef /= 10;
+}
+WriteLine(new_number);
