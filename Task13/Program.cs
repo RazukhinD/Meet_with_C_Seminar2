ec6323a928c2ba2cacf6f0004040cb0dd6c089a3
Dmitrii <razuhindm@gmail.com>
2022-09-19 22:49:44 +0300

    Домашняя задача №13

---
+++ Task13/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+Clear();
+WriteLine("Введите трехзначное число :");
+int number = int.Parse(ReadLine()!);
+int a2 = number/100;
+
+if (a2 ==0) 
+{
+    WriteLine("Такой цифры нет");
+    return;
+}
+
+
+int temp1 = number/100;
+a2 = temp1%10;
+WriteLine($" Третья цифра числа {a2}");
