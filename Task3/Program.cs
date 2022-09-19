cfd2f146f9cd4abfd6c3d8ffb54ea97fcce31560
Dmitrii <razuhindm@gmail.com>
2022-09-19 21:21:04 +0300

    Четвертая задача

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+Clear();
+WriteLine("Введи число: ");
+int number1 = int.Parse(ReadLine()!);
+
+if (number1 == 0)
+{
+    WriteLine(" Просто чтобы ты не баловался");
+    return;
+}
+
+int div1 = number1 % 7;
+int div2 = number1 % 23;
+
+if (div1 == 0 && div2 == 0)
+{
+    WriteLine($"{number1} кратно 7 и 23");
+}
+else
+{
+   WriteLine($"Число {number1} не кратно 7 и 23");
+}
