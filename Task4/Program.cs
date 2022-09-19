4f425f135344c2c9ef4cd8d1ec0d39748e4fc853
Dmitrii <razuhindm@gmail.com>
2022-09-19 21:28:32 +0300

    Пятая задача

---
+++ Task4/Program.cs
@@ -0,0 +1,37 @@
+﻿using static System.Console;
+
+Clear();
+WriteLine("Введи первое число: ");
+int number1 = int.Parse(ReadLine()!);
+WriteLine("Введи второе число: ");
+int number2 = int.Parse(ReadLine()!);
+
+if (number2 == 0)
+{
+    WriteLine(" 0 не является квадратом ни какого числа");
+    return;
+}
+
+if (number1 == 0)
+{
+    WriteLine(" 0 не является квадратом ни какого числа");
+    return;
+}
+
+if (number2>number1)
+{
+    int temp = number1;
+    number1 = number2;
+    number2 = temp;
+}
+
+int sqrt = number2*number2;
+
+if (sqrt == number1)
+{
+    WriteLine($"{number1} квадрат {number2}");
+}
+else
+{
+   WriteLine($"{number1} не является квадратом {number2}");
+}
