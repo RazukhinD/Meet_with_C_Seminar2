c76dc51f8029ae643c39a03574bd6c439b5b9ea8
Dmitrii <razuhindm@gmail.com>
2022-09-19 20:51:23 +0300

    Третья задача

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+Clear();
+WriteLine("Введи первое число: ");
+int number1 = int.Parse(ReadLine()!);
+WriteLine("Введи второе число: ");
+int number2 = int.Parse(ReadLine()!);
+
+if (number2 == 0)
+{
+    WriteLine(" На 0 делить нельзя");
+}
+else 
+{
+    int div = number1%number2;
+
+    if (div == 0)
+    {
+        WriteLine($"{number1} кратно {number2}");
+    }
+    else
+    {
+    WriteLine($"Остаток от деления {number1} на {number2}: {div}");
+    }
+}
