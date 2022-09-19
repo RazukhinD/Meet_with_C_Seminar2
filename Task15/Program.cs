43d75dccf5ba2f62a2d7992cdf7f8d039159bbf8
Dmitrii <razuhindm@gmail.com>
2022-09-19 22:55:29 +0300

    Домашняя задача №15

---
+++ Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+Clear();
+Write("Введите номер дня недели: ");
+int num1 = Convert.ToInt32(ReadLine()!);
+
+if (num1<1 || num1>7)
+{
+    WriteLine("Введи правильный номер");
+    return;
+}
+if (num1 == 6 || num1 == 7)
+{
+    WriteLine("Выходной");
+    return;
+}
+WriteLine("Будний");
